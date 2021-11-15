717121153b5df62a526e9952a3e48d174f6f3c75
CecileAnna <6263325@rts-365.dk>
2021-11-15 15:24:26 +0100

    working on API making arrays with articles commit

---
+++ js/news-api.js
@@ -0,0 +1,38 @@
+const urlHome = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=75CaAGwJshFzuxAiz5Q55fG4vFpYvbYB';
+
+let articleArray = [];
+
+let businessNewsArray = [];
+let europeNewsArray = [];
+let healthNewsArray = [];
+let sportsNewsArray = [];
+let travelNewsArray = [];
+
+axios.get(urlHome).then(response => {
+
+    articleArray = response.data.results;
+
+    // console.log(articleArray);
+
+    articleArray.forEach(el => {
+
+
+        if (el.section === 'business') {
+            businessNewsArray.push(el);
+        } else if (el.subsection === 'europe') {
+            europeNewsArray.push(el);
+        } else if (el.section === 'health') {
+            healthNewsArray.push(el);
+        } else if (el.section === 'sports') {
+            sportsNewsArray.push(el);
+        } else if (el.section === 'travel') {
+            travelNewsArray.push(el);
+        } else {
+            console.log('No articles matches any categories!')
+        };
+
+    });
+
+    console.log(europeNewsArray);
+
+});
