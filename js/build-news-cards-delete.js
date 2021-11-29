81ecf33b1e3db522dd3ea03d78d072b571a5ed6d
CecileAnna <6263325@rts-365.dk>
2021-11-29 10:17:35 +0100

    work on creating news cards dynamically

---
+++ js/build-news-cards-delete.js
@@ -0,0 +1,120 @@
+const newsCardsContainer = document.querySelector('#News-cards');
+// const newsCardsCategorySection = document.querySelector('.News-category');
+// const newsCardsCategoryCardsWrapper = document.querySelector('.News-category__cards-wrapper');
+let newsCategory;
+let categoryTitle;
+let newsTitle;
+let newsParagraf;
+
+//creating elements
+//for category part
+const newsCardsCategorySection = document.createElement('section');
+newsCardsCategorySection.classList.add('News-category');
+
+const categorySectionBar = document.createElement('section');
+categorySectionBar.classList.add('News-category__bar');
+categorySectionBar.setAttribute('id', `${newsCategory}`)
+
+const categorySectionContentWrapper = document.createElement('div');
+categorySectionContentWrapper.classList.add('News-category__content-wrapper');
+
+const categorySectionTitleWrapper = document.createElement('div');
+categorySectionTitleWrapper.classList.add('News-category__title-wrapper');
+
+const categorySectionTitleImg = document.createElement('img');
+categorySectionTitleImg.classList.add('News-category__img');
+categorySectionTitleImg.src = 'https://picsum.photos/100/100';
+categorySectionTitleImg.alt = 'Category picture';
+
+const categorySectionTitleH2 = document.createElement('h2');
+categorySectionTitleH2.classList.add('News-category__title', '_section-heading');
+categorySectionTitleH2 = `${categoryTitle}`;
+
+const categorySectionIconWrapper = document.createElement('div');
+categorySectionIconWrapper.classList.add('News-category__icon-wrapper');
+
+const categorySectionIconDown = document.createElement("i");
+categorySectionIconDown.classList.add('fas', 'fa-chevron-down', 'News-category__icon-down');
+
+const categorySectionIconRight = document.createElement("i");
+categorySectionIconRight.classList.add('fas', 'fa-chevron-right', 'News-category__icon-right');
+
+//for news card part
+const newsCardsCategoryCardsWrapper = document.createElement('div');
+newsCardsCategoryCardsWrapper.classList.add('News-category__cards-wrapper');
+
+const newsCard = document.createElement('section');
+newsCard.classList.add('News-card', 'animate__animated');
+
+const newsCardDeleteDiv = document.createElement('div');
+newsCardDeleteDiv.classList.add('News-card__delete-div');
+
+const newsCardDeleteIcon = document.createElement("i");
+newsCardDeleteIcon.classList.add('fas', 'fa-trash', 'News-card__delete-save-icon');
+
+const newsCardSaveDiv = document.createElement('div');
+newsCardSaveDiv.classList.add('News-card__save-div');
+
+const newsCardSaveIcon = document.createElement("i");
+newsCardSaveIcon.classList.add('fas', 'fa-trash', 'News-card__delete-save-icon');
+
+const newsCardFront = document.createElement('div');
+newsCardFront.classList.add('News-card__front', 'animate__animated');
+
+const newsCardImg = document.createElement('img');
+newsCardImg.classList.add('News-card__img');
+newsCardImg.src = 'https://picsum.photos/80/80';
+newsCardImg.alt = 'Article image';
+
+const newsCardTextWrapper = document.createElement('div');
+newsCardTextWrapper.classList.add('News-card__text-wrapper');
+
+const newsCardH3 = document.createElement('h3');
+newsCardH3.classList.add('News-card__h3', '_card-title');
+newsCardH3 = `${newsTitle}`;
+
+const newsCardP = document.createElement('p');
+newsCardP.classList.add('News-card__p', '_card-copy-small');
+newsCardP = `${newsParagraf}`;
+
+// appending parts
+
+newsCardTextWrapper.appendChild(newsCardP);
+
+newsCardTextWrapper.appendChild(newsCardH3);
+
+newsCardFront.appendChild(newsCardTextWrapper);
+
+newsCardFront.appendChild(newsCardImg);
+
+newsCardSaveDiv.appendChild(newsCardSaveIcon);
+
+newsCardDeleteDiv.appendChild(newsCardDeleteIcon);
+
+newsCard.appendChild(newsCardSaveDiv);
+
+newsCard.appendChild(newsCardDeleteDiv);
+
+newsCardsCategoryCardsWrapper.appendChild(newsCard);
+
+categorySectionBar.appendChild(newsCardsCategoryCardsWrapper);
+
+
+categorySectionIconWrapper.appendChild(categorySectionIconRight);
+
+categorySectionIconWrapper.appendChild(categorySectionIconDown);
+
+categorySectionTitleWrapper.appendChild(categorySectionTitleH2);
+
+categorySectionTitleWrapper.appendChild(categorySectionTitleImg);
+
+categorySectionContentWrapper.appendChild(categorySectionIconWrapper);
+
+categorySectionContentWrapper.appendChild(categorySectionTitleWrapper);
+
+
+categorySectionBar.appendChild(categorySectionContentWrapper);
+
+newsCardsCategorySection.appendChild(categorySectionBar);
+
+newsCardsContainer.appendChild(newsCardsCategorySection);
---
+++ js/swipe-functionality.js
@@ -0,0 +1,70 @@
+const newsCard = document.querySelector('.News-card');
+const newsCardDelete = document.querySelector('.News-card__delete-div');
+const newsCardArchive = document.querySelector('.News-card__save-div');
+const newsCardFront = document.querySelector('.News-card__front');
+
+let touchStartCoordinateX;
+let touchEndCoordinateX;
+let touchMoveCoordinateX;
+let deleteButtonWidth;
+
+newsCardFront.addEventListener('touchstart', (event) => {
+
+    touchStartCoordinateX = Math.floor(event.touches[0].clientX);
+
+    // console.log(touchStartCoordinateX);
+
+});
+
+newsCardFront.addEventListener('touchmove', (event) => {
+
+    touchMoveCoordinateX = Math.floor(event.touches[0].clientX);
+
+    // console.log(touchMoveCoordinateX);
+
+    deleteButtonWidth = (window.screen.width * 40) / 100;
+
+    if (touchMoveCoordinateX < touchStartCoordinateX && touchMoveCoordinateX > touchStartCoordinateX - deleteButtonWidth) {
+
+        newsCardFront.style.transform = `translateX(${touchMoveCoordinateX - touchStartCoordinateX}px)`;
+        newsCardFront.style.transition = `0.1s ease-in-out`;
+
+    }
+
+});
+
+newsCardFront.addEventListener('touchend', (event) => {
+
+    touchEndCoordinateX = Math.floor(event.changedTouches[0].clientX);
+
+    // console.log(touchEndCoordinateX);
+    // console.log(touchStartCoordinateX);
+    // console.log(deleteButtonWidth);
+
+    if (touchEndCoordinateX < touchStartCoordinateX - deleteButtonWidth / 2) {
+
+        // console.log('teeest');
+
+        newsCardFront.style.transform = `translateX(-${deleteButtonWidth}px)`;
+
+    } else {
+        newsCardFront.style.transform = `translateX(${0})`;
+    }
+
+});
+
+newsCardDelete.addEventListener("click", () => {
+
+    // swipeContainer.classList.add('animate__delay-1s');
+    // swipeContainer.classList.add('animate__slow');
+    newsCard.classList.add('animate__fadeOutLeft');
+
+    setTimeout(function() {
+        newsCard.classList.add('_collapsed');
+    }, 50);
+
+    setTimeout(function() {
+        newsCard.remove()
+    }, 1600);
+
+});
